366c5b35a9fe5e494d36779720d3723398da3f0e
Pol González Arribas <polgonzarri@gmail.com>
2025-12-04 18:04:32 +0100

    compara-numeros-1-6

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
-basic.forever(function () {
-	
+let numero1 = 0
+let numero2 = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    for (let index = 0; index < 3; index++) {
+        basic.showNumber(receivedNumber)
+        if (numero1 > numero2) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Sad)
+        }
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("String")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    numero1 = randint(1, 6)
+    radio.sendNumber(numero1)
+    numero2 = randint(1, 6)
+    basic.showNumber(numero2)
 })
